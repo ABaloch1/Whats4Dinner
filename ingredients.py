e4d2216e8c4ae57415e5bf76d0ddb0a3701e4d05
Joshua Krug <118022479+AusarTheVil3@users.noreply.github.com>
2024-04-21 19:13:29 -0400

    Update ingredients.py
    sending lists of ingredients to and from html

--- ingredients.py
+++ ingredients.py
@@ -52,28 +52,36 @@ def toggle_ingredient_pantry():
 				#retrieves the ingredients in a list according to category
 				categories = ['Carbs', 'Fruits', 'Vegetables', 'Grains', 'Meat', 'Seafood', 'Dairy & Eggs', 'Complementary', 'Misc']
 				all_ingrs = []
+				user_ingrs = []
 				
 				for category in categories:
 					cur.execute("SELECT * FROM Ingredients WHERE Category = %s", (category))
 					ingrs = cur.fetchall()
 					all_ingrs += ingrs
 					
-				return render_template('edit_recipe.html', all_ingrs=all_ingrs, )
+				cur.execute("SELECT * FROM Pantry WHERE Username = %s", (username,))
+				user_ingrs = cur.fetchall()
+					
+				return render_template('edit_recipe.html', all_ingrs=all_ingrs, user_ingrs=user_ingrs)
 		elif request.method == 'POST':
-				username = session['username']
-				selected_ingredients = request.form.getlist('ingredients')
+			 	username = session['username']
+			 	selected_ingredients = request.form.getlist('selected_ingredients')
 			 	
-				cur.execute("SELECT COUNT(*) FROM Ingredients WHERE Name = ?", (i_name,))
-				exists = cur.fetchone()[0]
-				if exists:
-					cur.execute("SELECT COUNT(*) FROM Pantry WHERE Username = %s AND Ingredient = %s", (username, name))
+			 	for ingredient in selected_ingredients:
+					cur.execute("SELECT COUNT(*) FROM Pantry WHERE Username = %s AND Ingredient = %s", (username, ingredient))
 					in_pantry = cur.fetchone()[0]
 					if not in_pantry:
-						cur.execute("INSERT INTO Pantry (Username, Ingredient_Name) VALUES (%s, %s)", (username, i_name))
+						cur.execute("INSERT INTO Pantry (Username, Ingredient_Name) VALUES (%s, %s)", (username, ingredient))
 						cnx.commit()
-					else:
-						cur.execute("DELETE FROM Pantry (Username, Ingredient_Name) VALUES (%s, %s)", (username, i_name))
+						
+				cur.execute("SELECT * FROM Pantry WHERE Username = %s", (username,))
+				user_ingrs = cur.fetchall()
+				
+				for ingr in user_ingrs:
+					if ingr[1] not in selected_ingredients:
+						cur.execute("DELETE FROM Pantry WHERE Username = %s AND Ingredient_Name = %s", (username, ingr[1]))
 						cnx.commit()
+					
 	except:
 		cnx.rollback()
 		return render_template('pantry.html')

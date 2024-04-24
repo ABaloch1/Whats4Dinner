1f408f876a7b9c90c49007f1cb45b238a0661ae3
Joshua Krug <118022479+AusarTheVil3@users.noreply.github.com>
2024-04-24 12:52:09 -0400

    fixing typos

--- pantry.py
+++ pantry.py
@@ -1,4 +1,4 @@
-dfrom flask import Flask, render_template, redirect, url_for, request, session, Blueprint
+from flask import Flask, render_template, redirect, url_for, request, session, Blueprint
 #from flask_mysqldb import MySQL
 import mysql.connector
 

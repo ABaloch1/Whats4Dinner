467783b56216076506ab603dd00dff4841abd418
Joshua Krug <118022479+AusarTheVil3@users.noreply.github.com>
2024-04-24 13:11:15 -0400

    try try again

--- pantry.py
+++ pantry.py
@@ -31,7 +31,6 @@ def pantry_page():
 @pantry.route('/update_pantry', methods=['GET', 'POST'])
 def update_pantry():
     try:
-
         config = {
             'user': session['username'],
             'password': session['password'],
@@ -41,46 +40,47 @@ def update_pantry():
         cnx = mysql.connector.connect(**config)
         cur = cnx.cursor(dictionary=True)
 
-        user_ingredients = []
-        cur.execute("SELECT Ingredient FROM User_Pantry WHERE Username = %s", (session['username'],))
-        user_ingredients = cur.fetchall()
-        user_ingredients = [ingredient[0] for ingredient in user_ingredients]
-
-        params = "RI.Ingredient = '" #string
-        for i, ingredient in enumerate(user_ingredients):
-            params += ingredient + "'"
-            if i < len(user_ingredients) - 1: #add except for when it's the last ingredient
-                params += "OR RI.Ingredient = "
-
-        cur.execute("SELECT R.Recipe_ID FROM Recipes R INNER JOIN Recipe_Ingredients RI ON R.Recipe_ID=RI.Recipe_ID WHERE %s", (params,))
-        possible_recipes = cur.fetchall()    
-        missing_ingredients_per_recipe = []
-        
-        for recipe_id in possible_recipes:
-            recipe_ingredients = []
-            cur.execute("SELECT Ingredient FROM Recipe_Ingredients WHERE Recipe_ID = %s", (recipe_id,))
-            recipe_ingredients = cur.fetchall()
-            recipe_ingredients = [ingredient[0] for ingredient in recipe_ingredients]
-            missing_ingredients = []
+        if request.method == 'POST':
+            user_ingredients = []
+            cur.execute("SELECT Ingredient FROM User_Pantry WHERE Username = %s", (session['username'],))
+            user_ingredients = cur.fetchall()
+            user_ingredients = [ingredient[0] for ingredient in user_ingredients]
+            
+            params = "RI.Ingredient = '" #string
+            for i, ingredient in enumerate(user_ingredients):
+                params += ingredient + "'"
+                if i < len(user_ingredients) - 1: #add except for when it's the last ingredient
+                    params += "OR RI.Ingredient = "
+            
+            cur.execute("SELECT R.Recipe_ID FROM Recipes R INNER JOIN Recipe_Ingredients RI ON R.Recipe_ID=RI.Recipe_ID WHERE %s", (params,))
+            possible_recipes = cur.fetchall()    
+            missing_ingredients_per_recipe = []
+            
+            for recipe_id in possible_recipes:
+                recipe_ingredients = []
+                cur.execute("SELECT Ingredient FROM Recipe_Ingredients WHERE Recipe_ID = %s", (recipe_id,))
+                recipe_ingredients = cur.fetchall()
+                recipe_ingredients = [ingredient[0] for ingredient in recipe_ingredients]
+                missing_ingredients = []
+            
             for ingredient in recipe_ingredients:
                 if ingredient not in user_ingredients:
                     missing_ingredients.append(ingredient)
             if missing_ingredients:
                 missing_ingredients_per_recipe.append((recipe_id, missing_ingredients))
+                
+            categories = ['Carbs', 'Fruits', 'Vegetables', 'Grains', 'Meat', 'Seafood', 'Dairy & Eggs', 'Complementary', 'Misc']
+        
+            # Initialize dictionary to hold ingredients for each category
+            categorized_ingredients = {category: [] for category in categories}
 
-        categories = ['Carbs', 'Fruits', 'Vegetables', 'Grains',
-                      'Meat', 'Seafood', 'Dairy & Eggs', 'Complementary', 'Misc']
-
-        # Initialize dictionary to hold ingredients for each category
-        categorized_ingredients = {category: [] for category in categories}
-
-        # Fetch all ingredients and categorize them
-        for category in categories:
-            cur.execute(
-                "SELECT * FROM Ingredients WHERE Category = %s", (category,))
-            ingredients = [ingredient[0] for ingredient in cur.fetchall()]
-            categorized_ingredients[category] = ingredients
-            print(ingredients)
+            # Fetch all ingredients and categorize them
+            for category in categories:
+                cur.execute(
+                    "SELECT * FROM Ingredients WHERE Category = %s", (category,))
+                ingredients = [ingredient[0] for ingredient in cur.fetchall()]
+                categorized_ingredients[category] = ingredients
+                print(ingredients)
 
         if request.method == 'POST':
             selected_ingredients = request.form.getlist('selected_ingredients')
